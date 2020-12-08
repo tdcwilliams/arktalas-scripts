8b31cd7f829029538fe5a01b2fc17a377a308a57
tdcwilliams <tdcwilliams@gmail.com>
2020-12-08 16:14:43 +0100

    make variables lower case so they will work with config files in python

--- python/slurm_templates/slurm.sh
+++ python/slurm_templates/slurm.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -x
-#SBATCH --account=%%{PROJ_NUM}
-#SBATCH --job-name=%%{JOB_NAME}
+#SBATCH --account=%%{proj_num}
+#SBATCH --job-name=%%{job_name}
 #SBATCH --output=logs/slurm.%j.log
-#SBATCH --time=%%{WALL_TIME}
-#SBATCH --nodes=%%{NUM_NODES}
-#SBATCH --ntasks=%%{NUM_TASKS}
+#SBATCH --time=%%{wall_time}
+#SBATCH --nodes=%%{num_nodes}
+#SBATCH --ntasks=%%{num_tasks}
 #SBATCH --cpus-per-task=1
-#SBATCH --qos=%%{QOS}
+#SBATCH --qos=%%{qos}
 ##SBATCH --partition=bigmem
 ##SBATCH --mem-per-cpu=4G
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=%%{EMAIL}
+#SBATCH --mail-user=%%{email}
 
 cd $SLURM_SUBMIT_DIR
-source %%{ENV_FILE}
+source %%{env_file}
 singularity exec --cleanenv $NEXTSIM_SIF \
     mpirun tmp/nextsim.exec --config-files=inputs/nextsim.cfg \
     &> "logs/nextsim.${SLURM_JOB_ID}.log" || exit 1
