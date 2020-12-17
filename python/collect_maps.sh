d4d37c5dc16b161387a4f52531de0be0ca67d664
tdcwilliams <tdcwilliams@gmail.com>
2020-12-17 11:09:54 +0100

    update collect_maps.sh and rename pylatex_nxs.py

--- python/collect_maps.sh
+++ python/collect_maps.sh
@@ -1,43 +1,31 @@
 #! /bin/bash -x
-# 
-# In new container do:
-# export DEBIAN_FRONTEND=noninteractive
-# apt-get update
-# apt-get install texlive-pictures texlive-science texlive-latex-extra
-# pip install pylatex
+# needs to run on a machine or inside a container
+# with python (specifically the pylatex library) and pdfunite installed
 
 [[ $# -ne 2 ]] && exit 1
-REMOTE_DIR=$1
-LOCAL_DIR=$2
+ROOT_DIR=$1
 
 # download the mean maps for observations in $obs
 obs=()
-obs+=(eval-cs2smos)
-obs+=(eval-osisaf-conc)
-obs+=(eval-osisaf-drift)
-obs+=(eval-osisaf-drift-mu10kpd)
+obs+=("eval-cs2smos")
+obs+=("eval-osisaf-conc")
+obs+=("eval-osisaf-drift")
+obs+=("eval-osisaf-drift-mu10kpd")
 
 pdf_list=()
-for obs_dir in "${obs[@]}"
+for sub_dir in "${obs[@]}"
 do
-    remote_dir=$REMOTE_DIR/$obs_dir
-    local_dir=$LOCAL_DIR/$obs_dir
-    for subdir in maps_bias maps_fcst maps_obs
-    do
-        mkdir -p $local_dir/$subdir
-        scp $remote_dir/$subdir/*mean* $local_dir/$subdir
-    done
-
-    # download the summary figs
-    scp $remote_dir/*.* $local_dir/
+    obs_dir=$ROOT_DIR/$sub_dir
+    [[ ! -d $obs_dir ]] && continue
 
     # collect figs into pdf
-    python pylatex_nxs.py $local_dir
-    pdf=$local_dir/collected_maps.pdf
+    ./collect_maps_pylatex.py $obs_dir
+    pdf=$obs_dir/collected_maps.pdf
     [[ -f $pdf ]] && pdf_list+=($pdf)
 done
 
 # merge into one pdf
-[[ ${#pdf_list[@]} -lt 2 ]] && exit 0
-suffix=$(basename $LOCAL_DIR)
-pdfunite ${pdf_list[@]} $LOCAL_DIR/collected_maps_$suffix.pdf
+[[ ${#pdf_list[@]} -eq 0 ]] && echo no pdf files created - quitting && exit 0
+outfile=$ROOT_DIR/collected_maps_$(basename $ROOT_DIR).pdf
+[[ ${#pdf_list[@]} -eq 1 ]] && cp ${pdf_list[0]} $outfile && exit 0
+pdfunite ${pdf_list[@]} $outfile
