53e3efef64c9401b257a212cf01aea1a92767607
tdcwilliams <tdcwilliams@gmail.com>
2021-03-02 12:07:41 +0100

    update with new code from Anton

--- python/RS2_drift/02_get_drift.py
+++ python/RS2_drift/02_get_drift.py
@@ -9,6 +9,7 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 from cartopy.mpl.gridliner import LATITUDE_FORMATTER, LONGITUDE_FORMATTER
 from matplotlib.tri import Triangulation
+from argparse import ArgumentParser
 
 from shapely.geometry import Polygon
 import pyproj
@@ -34,8 +35,42 @@ NS_CRS = ccrs.NorthPolarStereo(central_longitude=-45, true_scale_latitude=60,
 LAND_50M = cfeature.NaturalEarthFeature('physical', 'land', '50m',
                                         edgecolor='face',
                                         facecolor=cfeature.COLORS['land'])
-# threshold on quality = rpm*hpm
-QTHRESH = 3.5
+def parse_args():
+    parser = ArgumentParser('Extract sea ice drift from pairs of Radarsat 2 images')
+    parser.add_argument('outdir', type=str,
+            help='Where to save the figures')
+    parser.add_argument('-t', '--test', action='store_true',
+            help='Just run for one day for testing')
+    parser.add_argument('-urf', '--use-real-features', action='store_true',
+            help='Use real features for feature tracking')
+    parser.add_argument('-qt', '--quality-threshold', type=float,
+            default=2, help='Threshold on quality, require rpm*hpm > this value')
+    return parser.parse_args()
+
+def fake_feature_tracking(n1, n2):
+    # create fake feature tracking points
+    n1rows, n1cols = n1.shape()
+    n2rows, n2cols = n2.shape()
+
+    stp = 500
+    c1, r1 = [a.flatten() for a in np.meshgrid(
+        np.arange(100, n1cols-100, stp),
+        np.arange(100, n1rows-100, stp),
+        )]
+    c1lon, r1lat = n1.transform_points(c1, r1)
+    c2, r2 = n2.transform_points(c1lon, r1lat, DstToSrc=1)
+    gpi = (c2 > 0) * (c2 < n2cols) * (r2 > 0) * (r2 < n2rows)
+    return c1[gpi], r1[gpi], c2[gpi], r2[gpi]
+
+def clean_velo_field(args, a, rpm, hpm, fill_size=1, med_filt_size=3):
+    """ Replace gaps with median filtered values """
+    a2 = np.array(a)
+    a2[(rpm * hpm) < args.quality_threshold] = np.nan
+    a3 = fill_nan_gaps(a2, fill_size)
+    a4 = median_filter(a3, med_filt_size)
+    gpi = np.isnan(a2) * np.isfinite(a4) 
+    a2[gpi] = a4[gpi]
+    return a2
 
 def get_deformation_elems(x, y, u, v, a):
     """ Compute deformation for given elements.
@@ -169,7 +204,7 @@ def get_bbox():
     factor = 1.4
     return xav - factor*dx/2, xav + factor*dx/2, yav - factor*dy/2, yav + factor*dy/2
 
-def get_filename(t, index, n1, n2):
+def get_filename(args, t, index, n1, n2):
     fmt = '%Y%m%dT%H%M%SZ'
     return t.substitute(dict(
         index = index,
@@ -177,15 +212,15 @@ def get_filename(t, index, n1, n2):
         dto2 = n2.time_coverage_start.strftime(fmt),
         ))
 
-def save_fig(fig, t, index, n1, n2):
-    figname = get_filename(t, index, n1, n2)
+def save_fig(args, fig, t, index, n1, n2):
+    figname = get_filename(args, t, index, n1, n2)
     os.makedirs(os.path.dirname(figname), exist_ok=True)
     print(f'Saving {figname}')
     fig.savefig(figname, bbox_inches='tight')
     plt.close()
 
-def save_npz(t, index, n1, n2, **kwargs):
-    fname = get_filename(t, index, n1, n2)
+def save_npz(args, t, index, n1, n2, **kwargs):
+    fname = get_filename(args, t, index, n1, n2)
     os.makedirs(os.path.dirname(fname), exist_ok=True)
     print(f'Saving {fname}')
     np.savez(fname, **kwargs)
@@ -197,7 +232,7 @@ def get_projected_hh(n, d, **kwargs):
     n.undo()
     return n1_hh_pro.astype(float)
 
-def plot_ft(index, n1ft, n2ft, c1, r1, c2, r2):
+def plot_ft(args, index, n1ft, n2ft, c1, r1, c2, r2):
     lon1b, lat1b = n1ft.get_border()
     lon2b, lat2b = n2ft.get_border()
     x1b, y1b = NS_PROJ(lon1b, lat1b)
@@ -215,7 +250,7 @@ def plot_ft(index, n1ft, n2ft, c1, r1, c2, r2):
     ax.plot(lon2b, lat2b, '.-', label='border_2', transform=ccrs.PlateCarree())
     ax.legend()
     t = Template('out/ft_keypoints/ft_keypoints_${dto1}-${dto2}_${index}.png')
-    save_fig(fig, t, index, n1ft, n2ft)
+    save_fig(args, fig, t, index, n1ft, n2ft)
 
     # Plot ice drift on top of image_1
     # - end points in image_1 coordinate system
@@ -233,9 +268,9 @@ def plot_ft(index, n1ft, n2ft, c1, r1, c2, r2):
     plt.quiver(c1, r1, dc, dr, color='r', angles='xy', scale_units='xy')#, scale=0.2)
     plt.plot(n1lon2b, n1lat2b, 'k.-')
     t = Template('out/ft_quiver/ft_quiver_${dto1}-${dto2}_${index}.png')
-    save_fig(fig, t, index, n1ft, n2ft)
+    save_fig(args, fig, t, index, n1ft, n2ft)
 
-def plot_pm(index, n1ft, n2ft, lon1pm, lat1pm, upm, vpm, apm, rpm, hpm):
+def plot_pm(args, index, n1ft, n2ft, lon1pm, lat1pm, upm, vpm, apm, rpm, hpm):
     # compute ice drift speed [m/s]
     delta_t = (n2ft.time_coverage_start - n1ft.time_coverage_start).total_seconds()
     u = upm / delta_t
@@ -248,17 +283,17 @@ def plot_pm(index, n1ft, n2ft, lon1pm, lat1pm, upm, vpm, apm, rpm, hpm):
     ax = fig.add_subplot(121)
     quality = rpm*hpm
     im = ax.imshow(quality)
-    ax.contour(quality, levels=[QTHRESH])
+    ax.contour(quality, levels=[args.quality_threshold])
     fig.colorbar(im, shrink=.4)
     ax.set_title('rpm*hpm')
     #
     ax = fig.add_subplot(122)
     im = ax.imshow(u)
-    ax.contour(quality, levels=[QTHRESH])
+    ax.contour(quality, levels=[args.quality_threshold])
     ax.set_title('x velocity, m/s')
     fig.colorbar(im, shrink=.4)
     t = Template('out/pm_quality/pm_quality_${dto1}-${dto2}_${index}.png')
-    save_fig(fig, t, index, n1ft, n2ft)
+    save_fig(args, fig, t, index, n1ft, n2ft)
 
     # plot final drift on image 1
     res = 2000 #m
@@ -281,7 +316,7 @@ def plot_pm(index, n1ft, n2ft, lon1pm, lat1pm, upm, vpm, apm, rpm, hpm):
     hh[hh==0] = np.nan
 
     # plot valid vectors in stereographic projection
-    gpi = (quality > QTHRESH)
+    gpi = (quality > args.quality_threshold)
     fig = plt.figure(figsize=(20,20))
     ax = plt.axes(projection=NS_CRS)
     im = ax.imshow(hh, vmin=vmin, vmax=vmax, cmap='gray',
@@ -301,7 +336,7 @@ def plot_pm(index, n1ft, n2ft, lon1pm, lat1pm, upm, vpm, apm, rpm, hpm):
     ax.xaxis.set_major_formatter(LATITUDE_FORMATTER)
     ax.yaxis.set_major_formatter(LONGITUDE_FORMATTER)
     t = Template('out/pm_quiver/pm_quiver_${dto1}-${dto2}_${index}.png')
-    save_fig(fig, t, index, n1ft, n2ft)
+    save_fig(args, fig, t, index, n1ft, n2ft)
 
     # deformation
     e1, e2, e3, tri_a, tri_p, triangles = get_deformation_nodes(x1pm[gpi], y1pm[gpi], u[gpi], v[gpi])
@@ -334,29 +369,135 @@ def plot_pm(index, n1ft, n2ft, lon1pm, lat1pm, upm, vpm, apm, rpm, hpm):
             draw_labels=True, alpha=0.5, linestyle=':')
         ax.xaxis.set_major_formatter(LATITUDE_FORMATTER)
         ax.yaxis.set_major_formatter(LONGITUDE_FORMATTER)
-        save_fig(fig, t, index, n1ft, n2ft)
+        save_fig(args, fig, t, index, n1ft, n2ft)
 
-def process_1pair(f1, f2, index):
+def plot_pm_clean(args, index, n1ft, n2ft, lon1pm, lat1pm, upm, vpm, apm, rpm, hpm):
+    # compute ice drift speed [m/s]
+    delta_t = (n2ft.time_coverage_start - n1ft.time_coverage_start).total_seconds()
+    u = upm / delta_t
+    v = vpm / delta_t
+    u2 = clean_velo_field(args, u, rpm, hpm)
+    v2 = clean_velo_field(args, v, rpm, hpm)
+
+    # start points in stereoprojection
+    x1pm, y1pm = NS_PROJ(lon1pm, lat1pm)
+
+    ax = fig.add_subplot(121)
+    im = ax.imshow(u)
+    ax.set_title('x velocity, m/s')
+    fig.colorbar(im, shrink=.4)
+
+    ax = fig.add_subplot(121)
+    im = ax.imshow(u2)
+    ax.set_title('Cleaned x velocity, m/s')
+    fig.colorbar(im, shrink=.4)
+
+    t = Template('clean-u/u_${dto1}-${dto2}_${index}.png')
+    save_fig(args, fig, t, index, n1ft, n2ft)
+
+    # plot final drift on image 1
+    res = 2000 #m
+    xav = x1pm.mean()
+    yav = y1pm.mean()
+    xmin, xmax, ymin, ymax = x1pm.min(), x1pm.max(), y1pm.min(), y1pm.max()
+    dx = .2*(xmax-xav)
+    dy = .2*(ymax-yav)
+    xmin -= dx
+    xmax += dx
+    ymin -= dy
+    ymax += dy
+    d = Domain(NS_SRS, '-te %f %f %f %f -tr %f %f' % (
+        xmin, ymin, xmax, ymax, res, res))
+    d_extent = [xmin, xmax, ymin, ymax]
+    hh = n1ft[1]
+    (vmin,), (vmax,) = Figure(hh).clim_from_histogram(ratio=.9)
+    # n1pro = get_projected_hh(n1ft, d)# nearest neighbour
+    hh = get_projected_hh(n1ft, d, resample_alg=1)#1:bilinear - better than NN, tps=True
+    hh[hh==0] = np.nan
+
+    # plot valid vectors in stereographic projection
+    gpi = np.isfinite(u2*v2)
+    fig = plt.figure(figsize=(20,20))
+    ax = plt.axes(projection=NS_CRS)
+    im = ax.imshow(hh, vmin=vmin, vmax=vmax, cmap='gray',
+            origin='upper', zorder=0, extent=d_extent)
+    # plt.colorbar(im, shrink=0.5)
+    quiv = ax.quiver(x1pm[gpi], y1pm[gpi], u2[gpi], v2[gpi], rpm[gpi])#, scale=2)
+    fig.colorbar(quiv, shrink=0.5)
+    #plt.quiverkey(quiv, 110000, -770000, 0.05, '0.05 m/s', coordinates='data')
+    ax.set_title('Ice drift speed [m/s]')
+    ax.add_feature(LAND_50M, edgecolor='black')
+
+    ax.set_xlim([xmin, xmax])
+    ax.set_ylim([ymin, ymax])
+
+    ax.gridlines(linewidth=2, color='m', 
+        draw_labels=True, alpha=0.5, linestyle=':')
+    ax.xaxis.set_major_formatter(LATITUDE_FORMATTER)
+    ax.yaxis.set_major_formatter(LONGITUDE_FORMATTER)
+    t = Template('clean-quiver/quiver_${dto1}-${dto2}_${index}.png')
+    save_fig(args, fig, t, index, n1ft, n2ft)
+
+    # deformation
+    e1, e2, e3, tri_a, tri_p, triangles = get_deformation_nodes(x1pm[gpi], y1pm[gpi], u2[gpi], v2[gpi])
+    # convert deformations to %/day
+    e1 *= 8640000
+    e2 *= 8640000
+    e3 *= 8640000
+    for e,t,ttl in zip(
+            [e1,e2,e3],
+            [
+                Template('clean-shear/shear_${dto1}-${dto2}_${index}.png'),
+                Template('clean-divergence/divergence_${dto1}-${dto2}_${index}.png'),
+                Template('clean-total-defor/total-defor_${dto1}-${dto2}_${index}.png'),
+                ],
+            ['Shear [%/day]', 'Divergence [%/day]', 'Total deformation [%/day]']):
+        vmin, vmax = np.percentile(e, [20,80])
+        fig = plt.figure(figsize=(20,20))
+        ax = plt.axes(projection=NS_CRS)
+        im = ax.tripcolor(x1pm[gpi], y1pm[gpi],
+                          triangles=triangles, facecolors=e,
+                          vmin=vmin, vmax=vmax,
+                          edgecolors='k', linewidth=1)
+        fig.colorbar(im, shrink=0.5)
+        ax.set_title(ttl)
+        ax.add_feature(LAND_50M, edgecolor='black')
+
+        ax.set_xlim([xmin, xmax])
+        ax.set_ylim([ymin, ymax])
+
+        ax.gridlines(linewidth=2, color='m',
+            draw_labels=True, alpha=0.5, linestyle=':')
+        ax.xaxis.set_major_formatter(LATITUDE_FORMATTER)
+        ax.yaxis.set_major_formatter(LONGITUDE_FORMATTER)
+        save_fig(args, fig, t, index, n1ft, n2ft)
+
+def process_1pair(args, f1, f2, index):
     # create Nansat objects with one band only. 
     n1ft, n1pm = get_nansat(f1)
     n2ft, n2pm = get_nansat(f2)
     print(f'1st image start: {n1ft.time_coverage_start}')
     print(f'2nd image start: {n2ft.time_coverage_start}')
 
-    # Run Feature Tracking
-    # - get start/end coordinates in the image coordinate system (colums/rows)
-    # - works best with spatial mean removed (use `n1ft`, `n2ft`)
-    if 1:
-        #v1: feature tracking takes HH with spatial mean removed
-        c1, r1, c2, r2 = feature_tracking(n1ft, n2ft, nFeatures=100000,
-                ratio_test=0.6, domainMargin=0)
+    if args.use_real_features:
+        # Run Feature Tracking
+        # - get start/end coordinates in the image coordinate system (colums/rows)
+        # - works best with spatial mean removed (use `n1ft`, `n2ft`)
+        if 1:
+            #v1: feature tracking takes HH with spatial mean removed
+            c1, r1, c2, r2 = feature_tracking(n1ft, n2ft, nFeatures=100000,
+                    ratio_test=0.6, domainMargin=0)
+        else:
+            #v1: feature tracking takes HH without spatial mean removed
+            c1, r1, c2, r2 = feature_tracking(n1pm, n2pm, nFeatures=100000,
+                    ratio_test=0.6, domainMargin=0)
     else:
-        #v2: feature tracking takes HH without spatial mean removed
-        c1, r1, c2, r2 = feature_tracking(n1pm, n2pm, nFeatures=100000,
-                ratio_test=0.6, domainMargin=0)
+        # Fake feature tracking to get more starting points for pattern matching
+        c1, r1, c2, r2 = fake_feature_tracking(n1pm, n2pm)
+
     t = Template('out/npz_files/ft_${dto1}-${dto2}_${index}.npz')
-    save_npz(t, index, n1ft, n2ft, c1=c1, r1=r1, c2=c2, r2=r2)
-    plot_ft(index, n1ft, n2ft, c1, r1, c2, r2)
+    save_npz(args, t, index, n1ft, n2ft, c1=c1, r1=r1, c2=c2, r2=r2)
+    plot_ft(args, index, n1ft, n2ft, c1, r1, c2, r2)
 
     # Pattern matching
     # lon/lat grids for image_1
@@ -368,10 +509,23 @@ def process_1pair(f1, f2, index):
         min_border=300, max_border=300,
         img_size=50, srs=NS_SRS)
     t = Template('out/npz_files/pm_${dto1}-${dto2}_${index}.npz')
-    save_npz(t, index, n1ft, n2ft, upm=upm, vpm=vpm, apm=apm, rpm=rpm, hpm=hpm)
-    plot_pm(index, n1ft, n2ft, lon1pm, lat1pm, upm, vpm, apm, rpm, hpm)
+    save_npz(args, t, index, n1ft, n2ft, upm=upm, vpm=vpm, apm=apm, rpm=rpm, hpm=hpm)
+    plot_pm(args, index, n1ft, n2ft, lon1pm, lat1pm, upm, vpm, apm, rpm, hpm)
 
 def run():
+    args = parse_args()
+    rs2dir = os.getenv('RS2_dir')
+
+    if args.test:
+        # if testing just run 1 example
+        f1 = 'RS2_OK37499_PK364900_DK322205_SCWA_20130224_023727_HH_HV_SGF'
+        f2 = 'RS2_OK37500_PK364951_DK322250_SCWA_20130225_020811_HH_HV_SGF'
+        process_1pair(args,
+                os.path.join(rs2dir, f1),
+                os.path.join(rs2dir, f2),
+                0)
+        return
+
     df = pd.read_csv('out/RS2_pairs.csv',
             names=["File1","File2","Interval","Overlap"], skiprows=1,
             dtype={"File1":str, "File2":str, "Interval":float, "Overlap":float})
@@ -381,9 +535,9 @@ def run():
         print(f'Overlap = {overlap}')
         print(f'Time interval = {interval}')
         try:
-            process_1pair(
-                    os.path.join(os.getenv('RS2_dir'), f1),
-                    os.path.join(os.getenv('RS2_dir'), f2),
+            process_1pair(args,
+                    os.path.join(rs2dir, f1),
+                    os.path.join(rs2dir, f2),
                     index)
         except:
             continue
