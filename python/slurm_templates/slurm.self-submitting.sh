bd7536268b505a090bfe14f1cac76809d80c04b5
tdcwilliams <tdcwilliams@gmail.com>
2021-03-15 04:37:33 +0100

    clean slurm_templates/slurm.self-submitting.sh

--- python/slurm_templates/slurm.self-submitting.sh
+++ python/slurm_templates/slurm.self-submitting.sh
@@ -1,24 +1,18 @@
 #!/bin/bash -x
-## Project:
 #SBATCH --account=%%{proj_num}
-## Job name:
 #SBATCH --job-name=%%{job_name}
-## Output file
 #SBATCH --output=logs/slurm.%j.log   # Stdout & stderr
-## Wall time limit:
 #SBATCH --time=%%{wall_time}
-## Number of nodes:
 #SBATCH --nodes=%%{num_nodes}
-## Number of tasks (total)
 #SBATCH --ntasks=%%{num_tasks}
-## Set OMP_NUM_THREADS
 #SBATCH --cpus-per-task=1
-
-#SBATCH --qos=%%{qos}
 
 ## Email info
 #SBATCH --mail-type=ALL   # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=%%{email}
+
+SPLIT_LENGTH=%%{split_length}
+source %%{env_file}
 
 function _read_duration {
     tmp=(`grep "duration=" inputs/nextsim.cfg`)
@@ -32,9 +26,9 @@ function _read_duration {
 }
 
 function _check_duration {
-    # check running for $split_length days will not take us after $date_final
+    # check running for $SPLIT_LENGTH days will not take us after $date_final
     # - reduce $tmp_duration if so
-    tmp_duration=$split_length
+    tmp_duration=$SPLIT_LENGTH
     date_end=`date -d "$START + $tmp_duration days" "+%Y%m%d"`
     while [ $date_end -gt $date_final ]
     do
@@ -47,39 +41,60 @@ function _check_restart_file {
     # check if there is a restart file for start date
     # - if not, either use no restart or use [field,mesh]_final.[bin,dat]
     tmp=`grep "exporter_path=" inputs/nextsim.cfg`
-    _restart_path="${tmp#*=}/restart"
+    restart_path="${tmp#*=}/restart"
     restart_base="${START}T000000Z"
-    _restart_file="$_restart_path/field_${restart_base}.bin"
-    [[ ! -f $_restart_file ]] && restart_base="final"
+    _restart_file="$restart_path/field_${restart_base}.bin"
+    [[ ! -f $_restart_file ]] && unset restart_base
 }
 
-cd $SLURM_SUBMIT_DIR
-# get some template options from an external script
-source %%{env_file}
-split_length=%%{split_length}
-mpirun_cmd="mpirun tmp/nextsim.exec \
-    -mat_mumps_icntl_23 2000 \
-    --config-files=inputs/nextsim.cfg"
+function _check_start_date {
+    # check start date is allowed
+    [[ $START -lt $date_begin ]] && \
+        { echo "Bad input start date (<$date_begin)"; exit 1; }
+    [[ $START -ge $date_final ]] &&  \
+        { echo "Bad input start date (>$date_final)"; exit 1; }
+}
 
-# start and final day
+# init
+cd $SLURM_SUBMIT_DIR
+ns_opts=()
+
+# get $duration
+_read_duration
 tmp=`grep "time_init=" inputs/nextsim.cfg`
 date_begin=`date -d "${tmp#*=}" "+%Y%m%d"`
-_read_duration #read $duration
 date_final=`date -d "$date_begin + $duration days" "+%Y%m%d"`
-[[ -z $START ]] && { echo "Please enter start date with --export=START=yyyymmdd"; exit 1; }
-[[ $START -lt $date_begin ]] && { echo "Bad input start date (<$date_begin)"; exit 1; }
-[[ $START -ge $date_final ]] && { echo "Bad input start date (>$date_final)"; exit 1; }
-_check_duration # set date_end, tmp_duration
 
-# Special options for first day/restarts
-ns_opts=("--simul.duration=$tmp_duration")
-[[ $START -eq $date_begin ]] && ns_opts+=(--restart.start_from_restart=false)
-_check_restart_file # set restart_base
-ns_opts+=(--restart.basename=$restart_base)
+# get $START and check it
+START=${START-"$date_begin"}
+_check_start_date
+
+# set date_end, tmp_duration
+_check_duration
+ns_opts+=("--simul.duration=$tmp_duration")
+
+# set restart_base and restart_path
+# - default is set in nextsim.cfg
+_check_restart_file
+[[ ! -z $restart_base ]] && ns_opts+=(--restart.basename=$restart_base)
+if [ $START -gt $date_begin ]
+then
+    # Set extra options if not first day
+    ns_opts+=(--simul.spinup_duration=0)
+    ns_opts+=(--restart.start_from_restart=true)
+    ns_opts+=(--restart.input_path=$restart_path)
+    # assume we start from final if not on first day and restart base hasn't been set already
+    [[ -z $restart_base ]] && ns_opts+=(--restart.basename=final)
+fi
 
 # run the model
-$mpirun_cmd ${ns_opts[@]} &> "logs/nextsim.${SLURM_JOB_ID}.log" || exit 1
+mpirun -np $SLURM_NTASKS tmp/nextsim.exec \
+    --config-files=inputs/nextsim.cfg ${ns_opts[@]} \
+    &> "logs/nextsim.${SLURM_JOB_ID}.log" || exit 1
 
-# Only resubmit if we haven't reached $date_final
-[[ $date_end -lt $date_final ]] && sbatch --export=START=$date_end $0
+# Exit if we have reached $date_final
+[[ $date_end -eq $date_final ]] && exit 0
+
+# Otherwise resubmit and exit
+sbatch --export=START=$date_end $0
 exit $?
