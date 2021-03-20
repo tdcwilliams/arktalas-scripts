98798a02a99417fe44aaea4f04e557a2e3346fe4
tdcwilliams <tdcwilliams@gmail.com>
2021-03-20 23:23:01 +0100

    devel instead of bigmem

--- python/slurm_batch_job.sh
+++ python/slurm_batch_job.sh
@@ -2,9 +2,10 @@
 #SBATCH --account=nn2993k
 #SBATCH --job-name=JOBNAME
 #SBATCH --time 30:00:00
-#SBATCH --partition=bigmem
 #SBATCH --nodes=1 --ntasks-per-node=1 --cpus-per-task=1
-#SBATCH --mem-per-cpu=32G
+##SBATCH --partition=bigmem
+##SBATCH --mem-per-cpu=32G
+#SBATCH --qos=devel
 #SBATCH --output=logs/slurm.%j.out
 
 function usage {
