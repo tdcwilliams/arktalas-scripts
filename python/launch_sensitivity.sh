dea52a9b54c9b391e0781e317e09ef0230c54408
tdcwilliams <tdcwilliams@gmail.com>
2021-03-20 21:59:18 +0100

    minor change

--- python/launch_sensitivity.sh
+++ python/launch_sensitivity.sh
@@ -2,6 +2,12 @@
 function usage {
     echo Usage:
     echo "$(basename $0) ROOT_DIR BATCH_NAME CONFIG_FILE NS_CONFIG_FILE EXECUTABLE [LAUNCH]"
+    echo "ROOT_DIR : where expt_??? folders will be created"
+    echo "BATCH_NAME : slurm job names use this, as does the summarising csv file"
+    echo "CONFIG_FILE : for setup_sensitivity.py"
+    echo "NS_CONFIG_FILE : for nextsim executable"
+    echo "EXECUTABLE : path to nextsim executable to copy"
+    echo "LAUNCH : submit slurm jobs (1=default) or not (0)"
     exit 1
 }
 [[ $# -lt 5 ]] && usage
@@ -23,14 +29,13 @@ msg="Run summary in $runlist"
 [[ LAUNCH -eq 0 ]] && { echo ""; echo $msg; exit 0; }
 
 [[ ! -f $runlist ]] && { echo $runlist not found; exit 1; }
-runlist=($(cat $runlist))
+runlist=($(cat $runlist | grep -v Experiment))
 for run in "${runlist[@]}"
 do
     n=${#run}
     run_=${run#*,}
     n_=${#run_}
     edir=$ROOT_DIR/${run:0:$((n-n_-1))}
-    [[ "$edir" == "Experiment Directory" ]] && continue
     cd $edir && sbatch inputs/slurm.sh
 done
 
